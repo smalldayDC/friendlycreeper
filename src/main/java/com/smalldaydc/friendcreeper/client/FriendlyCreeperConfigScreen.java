eb71e3d8241d1de6b4eb79324d4c5bb36af07e85
smalldayDC <152234768+smalldayDC@users.noreply.github.com>
2026-04-30 19:18:16 +0800

    Refactor config screen with helper method

--- src/main/java/com/smalldaydc/friendcreeper/client/FriendlyCreeperConfigScreen.java
+++ src/main/java/com/smalldaydc/friendcreeper/client/FriendlyCreeperConfigScreen.java
@@ -9,6 +9,9 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 @Environment(EnvType.CLIENT)
 public class FriendlyCreeperConfigScreen {
 
@@ -20,91 +23,34 @@ public class FriendlyCreeperConfigScreen {
                 .setTitle(Text.translatable("config.friendcreeper.title"))
                 .setSavingRunnable(FriendlyCreeperConfig::save);
 
-        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+        ConfigEntryBuilder eb = builder.entryBuilder();
         ConfigCategory general = builder.getOrCreateCategory(Text.translatable("config.friendcreeper.category.general"));
         ConfigCategory client = builder.getOrCreateCategory(Text.translatable("config.friendcreeper.category.client"));
 
-        general.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.friendcreeper.allowOwnerDamage"),
-                        config.allowOwnerDamage)
-                .setDefaultValue(false)
-                .setTooltip(Text.translatable("config.friendcreeper.allowOwnerDamage.tooltip"))
-                .setSaveConsumer(value -> config.allowOwnerDamage = value)
-                .build());
+        // General
+        addBooleanEntry(general, eb, "allowOwnerDamage", false, () -> config.allowOwnerDamage, v -> config.allowOwnerDamage = v);
+        addBooleanEntry(general, eb, "followOwner", true, () -> config.followOwner, v -> config.followOwner = v);
+        addBooleanEntry(general, eb, "revengeOwner", true, () -> config.revengeOwner, v -> config.revengeOwner = v);
+        addBooleanEntry(general, eb, "snowGolemAttack", false, () -> config.snowGolemAttack, v -> config.snowGolemAttack = v);
+        addBooleanEntry(general, eb, "afraidOfCats", true, () -> config.afraidOfCats, v -> config.afraidOfCats = v);
 
-        general.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.friendcreeper.followOwner"),
-                        config.followOwner)
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("config.friendcreeper.followOwner.tooltip"))
-                .setSaveConsumer(value -> config.followOwner = value)
-                .build());
-
-        general.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.friendcreeper.revengeOwner"),
-                        config.revengeOwner)
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("config.friendcreeper.revengeOwner.tooltip"))
-                .setSaveConsumer(value -> config.revengeOwner = value)
-                .build());
-
-        general.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.friendcreeper.snowGolemAttack"),
-                        config.snowGolemAttack)
-                .setDefaultValue(false)
-                .setTooltip(Text.translatable("config.friendcreeper.snowGolemAttack.tooltip"))
-                .setSaveConsumer(value -> config.snowGolemAttack = value)
-                .build());
-
-        general.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.friendcreeper.afraidOfCats"),
-                        config.afraidOfCats)
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("config.friendcreeper.afraidOfCats.tooltip"))
-                .setSaveConsumer(value -> config.afraidOfCats = value)
-                .build());
-
-        client.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.friendcreeper.hurtSound"),
-                        config.hurtSound)
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("config.friendcreeper.hurtSound.tooltip"))
-                .setSaveConsumer(value -> config.hurtSound = value)
-                .build());
-
-        client.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.friendcreeper.renderPoppy"),
-                        config.renderPoppy)
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("config.friendcreeper.renderPoppy.tooltip"))
-                .setSaveConsumer(value -> config.renderPoppy = value)
-                .build());
-
-        client.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.friendcreeper.witherRoseOnLowHealth"),
-                        config.witherRoseOnLowHealth)
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("config.friendcreeper.witherRoseOnLowHealth.tooltip"))
-                .setSaveConsumer(value -> config.witherRoseOnLowHealth = value)
-                .build());
-
-        client.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.friendcreeper.tamedCreeperTexture"),
-                        config.tamedCreeperTexture)
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("config.friendcreeper.tamedCreeperTexture.tooltip"))
-                .setSaveConsumer(value -> config.tamedCreeperTexture = value)
-                .build());
+        // Client
+        addBooleanEntry(client, eb, "hurtSound", true, () -> config.hurtSound, v -> config.hurtSound = v);
+        addBooleanEntry(client, eb, "renderPoppy", true, () -> config.renderPoppy, v -> config.renderPoppy = v);
+        addBooleanEntry(client, eb, "witherRoseOnLowHealth", true, () -> config.witherRoseOnLowHealth, v -> config.witherRoseOnLowHealth = v);
+        addBooleanEntry(client, eb, "tamedCreeperTexture", true, () -> config.tamedCreeperTexture, v -> config.tamedCreeperTexture = v);
 
         return builder.build();
+    }
+
+    private static void addBooleanEntry(ConfigCategory category, ConfigEntryBuilder eb,
+                                        String key, boolean defaultValue,
+                                        Supplier<Boolean> getter, Consumer<Boolean> setter) {
+        category.addEntry(eb.startBooleanToggle(
+                Text.translatable("config.friendcreeper." + key), getter.get())
+                .setDefaultValue(defaultValue)
+                .setTooltip(Text.translatable("config.friendcreeper." + key + ".tooltip"))
+                .setSaveConsumer(setter)
+                .build());
     }
 }
